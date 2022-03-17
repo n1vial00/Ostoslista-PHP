a1e994717afc6faf7c563cc6ab6ec55fba2bcf14
Aleksi Viitanen <n1vial00@students.oamk.fi>
2022-03-17 13:31:31 +0200

    Repurposed code

--- index.php
+++ index.php
@@ -5,7 +5,7 @@ require_once "inc/functions.php";
 
 try {
 $db = openDB();
-$sql = "select * from task";
+$sql = "select * from item";
 $query = $db->query($sql);
 $results = $query->fetchAll(PDO::FETCH_ASSOC);
 header("HTTP/1.1 200 OK");
